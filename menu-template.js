87154d06ff37db1222e08f31f32fc0365972a0e8
informaticafecor <informaticafecor@gmail.com>
2025-06-25 11:55:28 -0500

    actualziar

--- menu-template.js
+++
@@ -1,8 +0,0 @@
-
-// template-loader.js 
-async function loadTemplate(templateName, targetId) { try { const response = await fetch(`${templateName}.html`); 
-const html = await response.text(); document.getElementById(targetId).innerHTML = html; } 
-catch (error) { console.error('Error cargando template:', error); } } document.addEventListener('DOMContentLoaded', () => { loadTemplate('menu', 'menu-container'); });
-
-
- 
